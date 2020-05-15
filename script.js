6f5af02745d01a45b111c8bab7137c642fbd3efd
hugo-levet <hugo.levet42130@gmail.com>
2020-05-16 00:41:49 +0200

    init

---
+++ script.js
@@ -0,0 +1,38 @@
+(function (globals) {
+    'use strict';
+    $(() => {
+        $('#password').bind('keyup', function () {
+            let securityLevel = 0;
+            let password = $(this).val();
+
+            // 1 lower char
+            let lowerRegex = /[a-z]/g;
+            if (password.match(lowerRegex)) {
+                securityLevel += 1;
+            }
+            // 1 upper char
+            let upperRegex = /[A-Z]/g;
+            if (password.match(upperRegex)) {
+                securityLevel += 1;
+            }
+            // 1 number char
+            let numberRegex = /[0-9]/g;
+            if (password.match(numberRegex)) {
+                securityLevel += 1;
+            }
+            // 1 special char
+            let noSpecialRegex = /^[0-9|a-zA-Z]*$/g;
+            if (!password.match(noSpecialRegex)) {
+                securityLevel += 1;
+            }
+            // 8 chars
+            if (password.length > 8) {
+                securityLevel += 1;
+            }
+
+            // $('#security_level').html('security : ' + securityLevel);
+            // change security level
+            $('#security_level').attr('class', 'level' + securityLevel);
+        });
+    });
+})();
